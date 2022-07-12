1e396789833e595eeb1f6f09440336348b67c22f
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2022-07-12 17:11:32 -0500

    Implementando hashing para creación de usuarios y clientes

--- services/customers.service.js
+++ services/customers.service.js
@@ -1,6 +1,15 @@
-const boom = require('@hapi/boom');
-const { models } = require('../libs/sequelize');
+// Archivo de servicios para "Clientes"
 
+// Se importa el gestor de errores
+const boom = require('@hapi/boom');
+
+// Se importa bcrypt
+const bcrypt = require('bcrypt');
+
+// Se importan los modelos para hacer CRUD
+const { models } = require('./../libs/sequelize');
+
+// Se crea una clase de Cliente con funciones para hacer CRUD
 class CustomerService {
 
   constructor() {}
@@ -20,10 +29,31 @@ class CustomerService {
     return user;
   }
 
+  // Función para creación de un cliente
   async create(data) {
-    const newCustomer = await models.Customer.create(data, {
+
+    // Se crea un hash de la contraseña y se hacen 10 saltos (Se encripta 10 veces)
+    const hash = await bcrypt.hash(data.user.password, 10);
+
+    // Se crea un clon de la data entrante y tambien se le crea un clon al objeto hijo que posee
+    // después al hijo se le edita el campo "password" a la contraseña encriptada
+    const NewData = {
+      ...data,
+      user:{
+        ...data.user,
+        password: hash
+      }
+    };
+
+    // Se crea el nuevo cliente con la información anteriormente clonada
+    const newCustomer = await models.Customer.create(NewData, {
       include: ['user']
     });
+
+    // Se elimina el atributo "password" por seguridad en lo que se le muestra al cliente luego de la creación
+    delete newCustomer.dataValues.user.dataValues.password;
+
+    // Se retorna la información resultante de la creación del cliente
     return newCustomer;
   }
 
--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,34 @@
+// Archivo de servicios para "Usuarios"
+
+// Se importa el gestor de errores
 const boom = require('@hapi/boom');
 
+// Se importa bcrypt
+const bcrypt = require('bcrypt');
+
+// Se importan los modelos para hacer CRUD
 const { models } = require('./../libs/sequelize');
 
+// Se crea una clase de Usuario con funciones para hacer CRUD
 class UserService {
   constructor() {}
 
+  // Función para creación de usuario
   async create(data) {
-    const newUser = await models.User.create(data);
+
+    // Se crea un hash de la contraseña y se hacen 10 saltos (Se encripta 10 veces)
+    const hash = await bcrypt.hash(data.password, 10);
+
+    // Se crea el usuario pero se clona el objeto de "data" entrante y se edita la contraseña por una encriptada
+    const newUser = await models.User.create({
+      ...data,
+      password: hash
+    });
+
+    // Se elimina el atributo "password" por seguridad en lo que se le muestra al usuario luego de la creación
+    delete newUser.dataValues.password;
+
+    // Se retorna los datos devueltos por la creación
     return newUser;
   }
 
